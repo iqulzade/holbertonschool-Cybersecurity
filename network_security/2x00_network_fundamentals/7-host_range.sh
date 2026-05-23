2879d14118e999e9118a87b67ebb87d2da1aed6f
İdris Quluzada <idrisquluzade07@gmail.com>
2026-05-23 21:01:44 +0400

    fix

--- network_security/2x00_network_fundamentals/7-host_range.sh
+++ network_security/2x00_network_fundamentals/7-host_range.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -c "import sys; ip=[int(x) for x in sys.argv[1].split('.')]; c=int(sys.argv[2]); ipn=(ip[0]<<24)|(ip[1]<<16)|(ip[2]<<8)|ip[3]; mask=(0xffffffff << (32-c)) & 0xffffffff; net=ipn & mask; bcast=net | (~mask & 0xffffffff); first=net+1; last=bcast-1; fmt=lambda x: '.'.join(str((x>>i)&255) for i in (24,16,8,0)); print(f'{fmt(first)} - {fmt(last)}')" "$1" "$2"
+m=$((0xffffffff << (32-$1) & 0xffffffff)); printf "%d.%d.%d.%d\n" $(( (m>>24)&255 )) $(( (m>>16)&255 )) $(( (m>>8)&255 )) $(( m&255 ))
